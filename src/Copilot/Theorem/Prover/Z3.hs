08d215b4e2c657e510ba5fb11b76b257f9be9c42
chathhorn <chathhorn@gmail.com>
2017-03-14 02:09:32 -0500

    Haskell!

--- src/Copilot/Theorem/Prover/Z3.hs
+++ src/Copilot/Theorem/Prover/Z3.hs
@@ -1,5 +1,3 @@
---------------------------------------------------------------------------------
-
 {-# LANGUAGE LambdaCase, NamedFieldPuns, FlexibleInstances, RankNTypes, GADTs,
     MultiParamTypeClasses, FlexibleContexts, DataKinds #-}
 {-# LANGUAGE Trustworthy #-}
@@ -16,19 +14,21 @@ import Copilot.Theorem.Prove (Output (..), check, Proof, Universal, Existential)
 import Copilot.Theorem.IL as IL
 import qualified Copilot.Theorem.Prove as P
 
-import Control.Monad (msum, mzero, when, void, unless)
+import Control.Monad (join, msum, mzero, when, void, unless, (>=>), liftM)
 import Control.Monad.State (StateT, runStateT, get, modify)
 import Control.Monad.Trans.Maybe (MaybeT (..))
 
-import Data.Word
-import Data.Maybe (fromJust, fromMaybe)
 import Data.Default (Default(..))
 import Data.List (foldl')
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Proxy
+import Data.Word
 
 import Data.Set (Set, (\\), union)
 import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.GADT.Compare
 
 import Language.SMTLib2 as SMT
 import Language.SMTLib2.Pipe
@@ -36,6 +36,7 @@ import Language.SMTLib2.Strategy
 import Language.SMTLib2.Debug
 
 import Language.SMTLib2.Pipe.Internals hiding (Var, vars)
+import Language.SMTLib2.Internals.Embed
 
 import System.Console.ANSI
 import System.IO
@@ -140,9 +141,7 @@ setSolver sid solver =
 getVars :: SolverId -> ProofScript TransState
 getVars sid = do
   vars <- vars <$> get
-  return $ case Map.lookup sid vars of
-    Nothing -> noVars
-    Just vs -> vs
+  return $ fromMaybe noVars (Map.lookup sid vars)
 
 setVars :: SolverId -> TransState -> ProofScript ()
 setVars sid vs =
@@ -156,9 +155,9 @@ newAssumps sid as' = do
       modify $ \s -> s { assumps = Map.insert sid (Set.fromList as') assumps }
       return as'
     Just as -> do
-      let as'' = (Set.fromList as') `union` as
+      let as'' = Set.fromList as' `union` as
       modify $ \s -> s { assumps = Map.insert sid as'' assumps }
-      return $ Set.elems $ (Set.fromList as') \\ as
+      return $ Set.elems $ Set.fromList as' \\ as
 
 deleteSolver :: SolverId -> ProofScript ()
 deleteSolver sid =
@@ -167,8 +166,8 @@ deleteSolver sid =
 startNewSolver :: SolverId -> ProofScript Solver
 startNewSolver sid = do
   pipe <- liftIO $ createPipe "z3" ["-smt2", "-in"]
-  dbg <- (options <$> get >>= return . debug)
-  s <- liftIO $ namedDebugBackend (show sid) (not dbg) pipe
+  dbg <- liftM debug (options <$> get)
+  let s = debugBackend' True (not dbg) (Just $ show sid) stdout pipe
   setSolver sid s
   return s
 
@@ -190,26 +189,26 @@ proofKind k = "k-induction (k = " ++ show k ++ ")"
 entailment :: SolverId -> [IL.Expr] -> [IL.Expr] -> ProofScript CheckSatResult
 entailment sid assumptions props = do
   s <- getSolver sid
-  liftIO $ withBackendExitCleanly s $ setOption (ProduceModels True)
+  liftIO $ withBackendExitCleanly (return s) $ setOption (ProduceModels True)
   -- liftIO $ withBackendExitCleanly s $ setOption (ProduceProofs True)
   -- liftIO $ withBackendExitCleanly s $ setOption (ProduceUnsatCores True)
   vs <- getVars sid
   assumps' <- newAssumps sid assumptions
-  (_, vs')  <- liftIO $ withBackendExitCleanly s $ runStateT (mapM_ (\e -> transB e >>= lift . assert) assumps') vs
+  (_, vs')  <- liftIO $ withBackendExitCleanly (return s) $ runStateT (mapM_ (transB >=> lift . assert) assumps') vs
   setVars sid vs'
-  liftIO $ withBackendExitCleanly s $ push
-  _ <- liftIO $ withBackendExitCleanly s $ runStateT
+  liftIO $ withBackendExitCleanly (return s) push
+  _ <- liftIO $ withBackendExitCleanly (return s) $ runStateT
     (transB (bsimpl (foldl' (Op2 Bool IL.Or) (ConstB False) $ map (Op1 Bool IL.Not) props)) >>= lift . assert) vs'
 
-  nraNL <- (options <$> get >>= return . nraNLSat)
+  nraNL <- liftM nraNLSat (options <$> get)
   res <- if nraNL
-    then liftIO $ withBackendExitCleanly s $ checkSat (Just (UsingParams (CustomTactic "qfnra-nlsat") []))
+    then liftIO $ withBackendExitCleanly (return s) $ checkSatWith (Just (UsingParams (CustomTactic "qfnra-nlsat") []))
          (CheckSatLimits (Just 5000) Nothing)
-    else liftIO $ withBackendExitCleanly s $ checkSat Nothing (CheckSatLimits (Just 5000) Nothing)
+    else liftIO $ withBackendExitCleanly (return s) $ checkSatWith Nothing (CheckSatLimits (Just 5000) Nothing)
 
-  when (res == Sat) $ void $ liftIO $ withBackendExitCleanly s $ getModel
-  -- when (res == Unsat) $ void $ liftIO $ withBackendExitCleanly s $ getProof
-  liftIO $ withBackendExitCleanly s $ pop
+  when (res == Sat) $ void $ liftIO $ withBackendExitCleanly (return s) getModel
+  -- when (res == Unsat) $ void $ liftIO $ withBackendExitCleanly (return s) $ getProof
+  liftIO $ withBackendExitCleanly (return s) pop
   -- liftIO $ print model
   return res
 
@@ -244,12 +243,12 @@ kInduction' startK maxK s as ps = (fromMaybe (Output P.Unknown ["proof by " ++ p
 
       entailment Base assumps [ConstB False] >>= \case
         Unknown -> unknown
-        Unsat   -> invalid $ "inconsistent assumptions"
+        Unsat   -> invalid "inconsistent assumptions"
         Sat     -> entailment Base (modelInit ++ base) baseInv >>= \case
           Sat     -> invalid $ "base case failed for " ++ proofKind k
           Unknown -> unknown
           Unsat   ->
-            if not inductive then valid ("proved without induction")
+            if not inductive then valid "proved without induction"
             else entailment Step step stepInv >>= \case
               Sat     -> unknown
               Unknown -> unknown
@@ -266,7 +265,7 @@ onlySat' s as ps = (fromJust . fst) <$> runPS (script <* stopSolvers) s
 
       entailment Base assumps [ConstB False] >>= \case
         Unknown -> unknown
-        Unsat   -> invalid $ "inconsistent assumptions"
+        Unsat   -> invalid "inconsistent assumptions"
         Sat     -> if inductive
           then unknown' "proposition requires induction to prove."
           else entailment Base (modelInit ++ base) (map (Op1 Bool IL.Not) baseInv) >>= \case
@@ -285,7 +284,7 @@ onlyValidity' s as ps = (fromJust . fst) <$> runPS (script <* stopSolvers) s
 
       entailment Base assumps [ConstB False] >>= \case
         Unknown -> unknown
-        Unsat   -> invalid $ "inconsistent assumptions"
+        Unsat   -> invalid "inconsistent assumptions"
         Sat     -> if inductive
           then unknown' "proposition requires induction to prove."
           else entailment Base (modelInit ++ base) baseInv >>= \case
@@ -320,16 +319,16 @@ getBoolVar :: String -> Trans (SMT.Expr Solver BoolType)
 getBoolVar = getVar bool boolVars (\v s -> s {boolVars  = v})
 
 getBV8Var  :: String -> Trans (SMT.Expr Solver (BitVecType 8))
-getBV8Var  = getVar (bitvec 8) bv8Vars  (\v s -> s {bv8Vars  = v})
+getBV8Var  = getVar (bitvec $ bw Proxy) bv8Vars  (\v s -> s {bv8Vars  = v})
 
 getBV16Var :: String -> Trans (SMT.Expr Solver (BitVecType 16))
-getBV16Var = getVar (bitvec 16) bv16Vars (\v s -> s {bv16Vars = v})
+getBV16Var = getVar (bitvec $ bw Proxy) bv16Vars (\v s -> s {bv16Vars = v})
 
 getBV32Var :: String -> Trans (SMT.Expr Solver (BitVecType 32))
-getBV32Var = getVar (bitvec 32) bv32Vars (\v s -> s {bv32Vars = v})
+getBV32Var = getVar (bitvec $ bw Proxy) bv32Vars (\v s -> s {bv32Vars = v})
 
 getBV64Var :: String -> Trans (SMT.Expr Solver (BitVecType 64))
-getBV64Var = getVar (bitvec 64) bv64Vars (\v s -> s {bv64Vars = v})
+getBV64Var = getVar (bitvec $ bw Proxy) bv64Vars (\v s -> s {bv64Vars = v})
 
 getRealVar  :: String -> Trans (SMT.Expr Solver RealType)
 getRealVar  = getVar real realVars (\v s -> s {realVars  = v})
@@ -344,92 +343,94 @@ getVar t proj upd v = do
       return newVar
     Just x -> return x
 
+munge = join
+
 transB :: IL.Expr -> Trans (SMT.Expr Solver BoolType)
 transB = \case
-  ConstB b           -> return $ constant b
-  Ite _ c e1 e2      -> ite <$> transB c <*> transB e1 <*> transB e2
-  Op1 _ IL.Not e     -> not' <$> transB e
-  Op2 _ IL.And e1 e2 -> (.&.) <$> transB e1 <*> transB e2
-  Op2 _ IL.Or e1 e2  -> (.|.) <$> transB e1 <*> transB e2
-  Op2 _ IL.Eq e1 e2  -> case typeOf e1 of
+  ConstB b           -> cbool b
+  Ite _ c e1 e2      -> munge $ ite <$> transB c <*> transB e1 <*> transB e2
+  Op1 _ IL.Not e     -> munge $ not' <$> transB e
+  Op2 _ IL.And e1 e2 -> munge $ (.&.) <$> transB e1 <*> transB e2
+  Op2 _ IL.Or e1 e2  -> munge $ (.|.) <$> transB e1 <*> transB e2
+  Op2 _ IL.Eq e1 e2  -> munge $ case typeOf e1 of
     Bool   -> (.==.) <$> transB e1    <*> transB e2
     Real   -> (.==.) <$> transR e1    <*> transR e2
     BV8    -> (.==.) <$> transBV8 e1  <*> transBV8 e2
     BV16   -> (.==.) <$> transBV16 e1 <*> transBV16 e2
     BV32   -> (.==.) <$> transBV32 e1 <*> transBV32 e2
-    BV64   -> (.==.) <$> transBV64 e1 <*> transBV64 e2
+    -- BV64   -> (.==.) <$> transBV64 e1 <*> transBV64 e2
     SBV8   -> (.==.) <$> transBV8 e1  <*> transBV8 e2
     SBV16  -> (.==.) <$> transBV16 e1 <*> transBV16 e2
     SBV32  -> (.==.) <$> transBV32 e1 <*> transBV32 e2
-    SBV64  -> (.==.) <$> transBV64 e1 <*> transBV64 e2
-  e@(Op2 _ Le e1 e2) -> case typeOf e1 of
+    -- SBV64  -> (.==.) <$> transBV64 e1 <*> transBV64 e2
+  e@(Op2 _ Le e1 e2) -> munge $ case typeOf e1 of
     Bool   -> error $ "Comparing Bools: " ++ show e
     Real   -> (.<=.) <$> transR e1    <*> transR e2
     BV8    -> bvule  <$> transBV8 e1  <*> transBV8 e2
     BV16   -> bvule  <$> transBV16 e1 <*> transBV16 e2
     BV32   -> bvule  <$> transBV32 e1 <*> transBV32 e2
-    BV64   -> bvule  <$> transBV64 e1 <*> transBV64 e2
+    -- BV64   -> bvule  <$> transBV64 e1 <*> transBV64 e2
     SBV8   -> bvule  <$> transBV8 e1  <*> transBV8 e2
     SBV16  -> bvule  <$> transBV16 e1 <*> transBV16 e2
     SBV32  -> bvule  <$> transBV32 e1 <*> transBV32 e2
-    SBV64  -> bvule  <$> transBV64 e1 <*> transBV64 e2
-  e@(Op2 _ Ge e1 e2) -> case typeOf e1 of
+    -- SBV64  -> bvule  <$> transBV64 e1 <*> transBV64 e2
+  e@(Op2 _ Ge e1 e2) -> munge $ case typeOf e1 of
     Bool   -> error $ "Comparing Bools: " ++ show e
     Real   -> (.>=.) <$> transR e1    <*> transR e2
     BV8    -> bvuge  <$> transBV8 e1  <*> transBV8 e2
     BV16   -> bvuge  <$> transBV16 e1 <*> transBV16 e2
     BV32   -> bvuge  <$> transBV32 e1 <*> transBV32 e2
-    BV64   -> bvuge  <$> transBV64 e1 <*> transBV64 e2
+    -- BV64   -> bvuge  <$> transBV64 e1 <*> transBV64 e2
     SBV8   -> bvuge  <$> transBV8 e1  <*> transBV8 e2
     SBV16  -> bvuge  <$> transBV16 e1 <*> transBV16 e2
     SBV32  -> bvuge  <$> transBV32 e1 <*> transBV32 e2
-    SBV64  -> bvuge  <$> transBV64 e1 <*> transBV64 e2
-  e@(Op2 _ Lt e1 e2) -> case typeOf e1 of
+    -- SBV64  -> bvuge  <$> transBV64 e1 <*> transBV64 e2
+  e@(Op2 _ Lt e1 e2) -> munge $ case typeOf e1 of
     Bool   -> error $ "Comparing Bools: " ++ show e
     Real   -> (.<.) <$> transR e1    <*> transR e2
     BV8    -> bvult <$> transBV8 e1  <*> transBV8 e2
     BV16   -> bvult <$> transBV16 e1 <*> transBV16 e2
     BV32   -> bvult <$> transBV32 e1 <*> transBV32 e2
-    BV64   -> bvult <$> transBV64 e1 <*> transBV64 e2
+    -- BV64   -> bvult <$> transBV64 e1 <*> transBV64 e2
     SBV8   -> bvult <$> transBV8 e1  <*> transBV8 e2
     SBV16  -> bvult <$> transBV16 e1 <*> transBV16 e2
     SBV32  -> bvult <$> transBV32 e1 <*> transBV32 e2
-    SBV64  -> bvult <$> transBV64 e1 <*> transBV64 e2
-  e@(Op2 _ Gt e1 e2) -> case typeOf e1 of
+    -- SBV64  -> bvult <$> transBV64 e1 <*> transBV64 e2
+  e@(Op2 _ Gt e1 e2) -> munge $ case typeOf e1 of
     Bool   -> error $ "Comparing Bools: " ++ show e
     Real   -> (.>.) <$> transR e1    <*> transR e2
     BV8    -> bvugt <$> transBV8 e1  <*> transBV8 e2
     BV16   -> bvugt <$> transBV16 e1 <*> transBV16 e2
     BV32   -> bvugt <$> transBV32 e1 <*> transBV32 e2
-    BV64   -> bvugt <$> transBV64 e1 <*> transBV64 e2
+    -- BV64   -> bvugt <$> transBV64 e1 <*> transBV64 e2
     SBV8   -> bvugt <$> transBV8 e1  <*> transBV8 e2
     SBV16  -> bvugt <$> transBV16 e1 <*> transBV16 e2
     SBV32  -> bvugt <$> transBV32 e1 <*> transBV32 e2
-    SBV64  -> bvugt <$> transBV64 e1 <*> transBV64 e2
+    -- SBV64  -> bvugt <$> transBV64 e1 <*> transBV64 e2
   SVal _ s i         -> getBoolVar $ ncVar s i
   e                  -> error $ "Encountered unhandled expression (Bool): " ++ show e
 
-ncVar :: [Char] -> SeqIndex -> [Char]
+ncVar :: String -> SeqIndex -> String
 ncVar s (Fixed i) = s ++ "_" ++ show i
 ncVar s (Var   i) = s ++ "_n" ++ show i
 
 transR :: IL.Expr -> Trans (SMT.Expr Solver RealType)
 transR = \case
-  ConstR n         -> return $ constant $ toRational n
-  Ite _ c e1 e2    -> ite <$> transB c <*> transR e1 <*> transR e2
+  ConstR n         -> creal $ toRational n
+  Ite _ c e1 e2    -> munge $ ite <$> transB c <*> transR e1 <*> transR e2
 
-  Op1 _ IL.Neg e   -> app neg <$> transR e
-  Op1 _ IL.Abs e   -> app SMT.Abs <$> transR e
+  Op1 _ IL.Neg e   -> munge $ neg <$> transR e
+  Op1 _ IL.Abs e   -> munge $ abs' <$> transR e
 
-  Op2 _ Add e1 e2  -> (\x y -> app plus [x, y]) <$> transR e1 <*> transR e2
-  Op2 _ Sub e1 e2  -> (\x y -> app minus (x, y)) <$> transR e1 <*> transR e2
-  Op2 _ Mul e1 e2  -> (\x y -> app mult [x, y]) <$> transR e1 <*> transR e2
-  Op2 _ Fdiv e1 e2 -> SMT.Div <$> transR e1 <*> transR e2
+  Op2 _ Add e1 e2  -> munge $ (.+.) <$> transR e1 <*> transR e2
+  Op2 _ Sub e1 e2  -> munge $ (.-.) <$> transR e1 <*> transR e2
+  Op2 _ Mul e1 e2  -> munge $ (.*.) <$> transR e1 <*> transR e2
+  Op2 _ Fdiv e1 e2 -> munge $ (./.) <$> transR e1 <*> transR e2
 
   Op2 _ Pow e1 e2  -> do
     -- let pow = SMTBuiltIn "^" () :: SMTFunction (SMT.Expr Solver Rational, SMT.Expr Solver Rational) Rational
     let pow = undefined -- TODO(chathhorn)
-    (\x y -> app pow (x, y)) <$> transR e1 <*> transR e2
+    pow <$> transR e1 <*> transR e2
 
   SVal _ s i       -> getRealVar $ ncVar s i
   e                -> error $ "Encountered unhandled expression (Rat): " ++ show e
@@ -437,48 +438,49 @@ transR = \case
 -- TODO(chathhorn): bleghh
 transBV8 :: IL.Expr -> Trans (SMT.Expr Solver (BitVecType 8))
 transBV8 = \case
-  ConstI _ n      -> return $ constant $ bitvec n
-  Ite _ c e1 e2   -> ite <$> transB c <*> transBV8 e1 <*> transBV8 e2
-  Op1 _ IL.Abs e  -> abs <$> transBV8 e
-  Op1 _ IL.Neg e  -> negate <$> transBV8 e
-  Op2 _ Add e1 e2 -> (+) <$> transBV8 e1 <*> transBV8 e2
-  Op2 _ Sub e1 e2 -> (-) <$> transBV8 e1 <*> transBV8 e2
-  Op2 _ Mul e1 e2 -> (*) <$> transBV8 e1 <*> transBV8 e2
+  ConstI _ n      -> cbv n $ bw Proxy
+  Ite _ c e1 e2   -> munge $ ite <$> transB c <*> transBV8 e1 <*> transBV8 e2
+  -- Op1 _ IL.Abs e  -> munge $ abs' <$> transBV8 e
+  Op1 _ IL.Neg e  -> munge $ bvneg <$> transBV8 e
+  Op2 _ Add e1 e2 -> munge $ bvadd <$> transBV8 e1 <*> transBV8 e2
+  Op2 _ Sub e1 e2 -> munge $ bvsub <$> transBV8 e1 <*> transBV8 e2
+  Op2 _ Mul e1 e2 -> munge $ bvmul <$> transBV8 e1 <*> transBV8 e2
   SVal _ s i      -> getBV8Var $ ncVar s i
   e               -> error $ "Encountered unhandled expression (BV8): " ++ show e
 
 transBV16 :: IL.Expr -> Trans (SMT.Expr Solver (BitVecType 16))
 transBV16 = \case
-  ConstI _ n      -> return $ constant $ bitvec n
-  Ite _ c e1 e2   -> ite <$> transB c <*> transBV16 e1 <*> transBV16 e2
-  Op1 _ IL.Abs e  -> abs <$> transBV16 e
-  Op1 _ IL.Neg e  -> negate <$> transBV16 e
-  Op2 _ Add e1 e2 -> (+) <$> transBV16 e1 <*> transBV16 e2
-  Op2 _ Sub e1 e2 -> (-) <$> transBV16 e1 <*> transBV16 e2
-  Op2 _ Mul e1 e2 -> (*) <$> transBV16 e1 <*> transBV16 e2
+  ConstI _ n      -> cbv n $ bw Proxy
+  Ite _ c e1 e2   -> munge $ ite <$> transB c <*> transBV16 e1 <*> transBV16 e2
+  -- Op1 _ IL.Abs e  -> munge $ abs' <$> transBV16 e
+  Op1 _ IL.Neg e  -> munge $ bvneg <$> transBV16 e
+  Op2 _ Add e1 e2 -> munge $ bvadd <$> transBV16 e1 <*> transBV16 e2
+  Op2 _ Sub e1 e2 -> munge $ bvsub <$> transBV16 e1 <*> transBV16 e2
+  Op2 _ Mul e1 e2 -> munge $ bvmul <$> transBV16 e1 <*> transBV16 e2
   SVal _ s i      -> getBV16Var $ ncVar s i
   e               -> error $ "Encountered unhandled expression (BV16): " ++ show e
 
 transBV32 :: IL.Expr -> Trans (SMT.Expr Solver (BitVecType 32))
 transBV32 = \case
-  ConstI _ n      -> return $ constant $ bitvec n
-  Ite _ c e1 e2   -> ite <$> transB c <*> transBV32 e1 <*> transBV32 e2
-  Op1 _ IL.Abs e  -> abs <$> transBV32 e
-  Op1 _ IL.Neg e  -> negate <$> transBV32 e
-  Op2 _ Add e1 e2 -> (+) <$> transBV32 e1 <*> transBV32 e2
-  Op2 _ Sub e1 e2 -> (-) <$> transBV32 e1 <*> transBV32 e2
-  Op2 _ Mul e1 e2 -> (*) <$> transBV32 e1 <*> transBV32 e2
+  ConstI _ n      -> cbv n $ bw Proxy
+  Ite _ c e1 e2   -> munge $ ite <$> transB c <*> transBV32 e1 <*> transBV32 e2
+  -- Op1 _ IL.Abs e  -> munge $ abs' <$> transBV32 e
+  Op1 _ IL.Neg e  -> munge $ bvneg <$> transBV32 e
+  Op2 _ Add e1 e2 -> munge $ bvadd <$> transBV32 e1 <*> transBV32 e2
+  Op2 _ Sub e1 e2 -> munge $ bvsub <$> transBV32 e1 <*> transBV32 e2
+  Op2 _ Mul e1 e2 -> munge $ bvmul <$> transBV32 e1 <*> transBV32 e2
   SVal _ s i      -> getBV32Var $ ncVar s i
   e               -> error $ "Encountered unhandled expression (BV32): " ++ show e
 
 transBV64 :: IL.Expr -> Trans (SMT.Expr Solver (BitVecType 64))
 transBV64 = \case
-  ConstI _ n      -> return $ constant $ bitvec n
-  Ite _ c e1 e2   -> ite <$> transB c <*> transBV64 e1 <*> transBV64 e2
-  Op1 _ IL.Abs e  -> abs <$> transBV64 e
-  Op1 _ IL.Neg e  -> negate <$> transBV64 e
-  Op2 _ Add e1 e2 -> (+) <$> transBV64 e1 <*> transBV64 e2
-  Op2 _ Sub e1 e2 -> (-) <$> transBV64 e1 <*> transBV64 e2
-  Op2 _ Mul e1 e2 -> (*) <$> transBV64 e1 <*> transBV64 e2
+  ConstI _ n      -> cbv n $ bw Proxy
+  Ite _ c e1 e2   -> munge $ ite <$> transB c <*> transBV64 e1 <*> transBV64 e2
+  -- Op1 _ IL.Abs e  -> munge $ abs' <$> transBV64 e
+  Op1 _ IL.Neg e  -> munge $ bvneg <$> transBV64 e
+  Op2 _ Add e1 e2 -> munge $ bvadd <$> transBV64 e1 <*> transBV64 e2
+  Op2 _ Sub e1 e2 -> munge $ bvsub <$> transBV64 e1 <*> transBV64 e2
+  Op2 _ Mul e1 e2 -> munge $ bvmul <$> transBV64 e1 <*> transBV64 e2
   SVal _ s i      -> getBV64Var $ ncVar s i
   e               -> error $ "Encountered unhandled expression (BV64): " ++ show e
+
